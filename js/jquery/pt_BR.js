55add8e82b3e284a7ebdb82c5adce0052416ac14
Rafael Gomide Bernades <gomide@hotmail.com.br>
2016-07-10 17:25:14 -0300

    Update Js pt_BR.js
    Adicionei as iniciais do dia da semana no arquivo de tradução.

---
+++ js/jquery/pt_BR.js
@@ -0,0 +1,18 @@
+// Brazilian Portuguese
+
+jQuery.extend( jQuery.fn.pickadate.defaults, {
+    monthsFull: [ 'janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro' ],
+    monthsShort: [ 'jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez' ],
+    weekdaysFull: [ 'domingo', 'segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira', 'sábado' ],
+    weekdaysLetter: [ 'D', 'S', 'T', 'Q', 'Q', 'S', 'S' ],
+    weekdaysShort: [ 'dom', 'seg', 'ter', 'qua', 'qui', 'sex', 'sab' ],
+    today: 'hoje',
+    clear: 'limpar',
+    close: 'fechar',
+    format: 'dd/mm/yyyy',
+    formatSubmit: 'dd/mm/yyyy'
+});
+
+jQuery.extend( jQuery.fn.pickatime.defaults, {
+    clear: 'limpar'
+});
