aa6c9f4f434ad487a18f963d3c4ca2fd22cd7105
Rafael Gomide Bernades <gomide@hotmail.com.br>
2016-07-11 00:22:16 -0300

    Update consulta
    Adicionei o atributo fornecedor no insert e a quantidade na tabela
    produtos.

--- api/index.php
+++ api/index.php
@@ -23,6 +23,7 @@ $app->post('/cadOs','auth',  function () use ($app, $db) {
         $km = $data[0]->km;
         $date = $data[0]->date;
         $obs = $data[0]->obs;
+        $fornecedor = $data[0]->fornecedor;
         $usuario = $_SESSION['usuario'];
         $nova_date = implode("-", array_reverse(explode("/", $date)));
 
@@ -32,13 +33,14 @@ $app->post('/cadOs','auth',  function () use ($app, $db) {
 
         
         $consulta = $db->con()->prepare('INSERT INTO os (
-           USU_IN_CODIGO, OS_IN_KM, VEI_IN_CODIGO, OS_TX_OBS, OS_DT_CADASTRO) 
-           VALUES (:USUARIO, :KM, :VEI, :OBS, :DATE)');
+           USU_IN_CODIGO, OS_IN_KM, VEI_IN_CODIGO, OS_TX_OBS, OS_DT_CADASTRO, FOR_IN_CODIGO) 
+           VALUES (:USUARIO, :KM, :VEI, :OBS, :DATE, :FORNECEDOR)');
         $consulta->bindParam(':USUARIO', $usuario);
         $consulta->bindParam(':KM', $km);
         $consulta->bindParam(':OBS', $obs);
         $consulta->bindParam(':VEI', $placa);
         $consulta->bindParam(':DATE', $nova_date);
+        $consulta->bindParam(':FORNECEDOR', $fornecedor);
   
     
         if($consulta->execute() == 1){
@@ -47,12 +49,12 @@ $app->post('/cadOs','auth',  function () use ($app, $db) {
             $dados = "";
             foreach($data[1] as $produto){   
               if($i != 0){ $dados .= ",";}
-              $dados .= "('" . $idOs . "', '" . $produto->id . "', '" . $produto->valor . "')";            
+              $dados .= "('" . $idOs . "', '" . $produto->id . "', '" . $produto->valor . "', '" . $produto->quantidade . "')";            
               $i++;  
             }
 
             $consulta = $db->con()->prepare('INSERT INTO itens_os (
-                       OS_IN_CODIGO, PRO_IN_CODIGO, ITE_OS_FL_VALOR) 
+                       OS_IN_CODIGO, PRO_IN_CODIGO, ITE_OS_FL_VALOR, ITE_OS_IN_QTD) 
                        VALUES '.$dados);                
             if($consulta->execute() == 1){
                 $db->con()->commit();  
