f8b9ca7d5676746051bd4166a4f41e8624aedb8d
Rafael Gomide Bernades <gomide@hotmail.com.br>
2016-07-11 22:45:04 -0300

    Update app
    Adicionei o modulo highcharts-ng.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
-var app = angular.module('app',['ngRoute','ui.mask'] );
+var app = angular.module('app',['ngRoute','ui.mask','highcharts-ng'] );
 app.config(function($routeProvider){  
 
     $routeProvider.
     when('/cadastroOs',{controller:'cadastroOsController', 
             templateUrl:'tlp/cadastro_os.html'}).
+    when('/',{controller:'dashController', 
+            templateUrl:'tlp/dash.html'}).
     otherwise({redirectTo:'/'});
 });
