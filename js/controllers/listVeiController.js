eb0fd49f166cd8d0b9b384670af892131100e97a
Rafael Gomide Bernades <gomide@hotmail.com.br>
2016-07-13 21:08:16 -0300

    Update controller
    Funçao para a consulta de exclusão do veiculo

--- js/controllers/listVeiController.js
+++ js/controllers/listVeiController.js
@@ -1,5 +1,6 @@
 app.controller('listVeiController',function($scope, $http){
     
+    $scope.listVeiculos = function(){
         console.log('listar veiculos');
         
         $http.get('api/dadosCadOs/veiculos')
@@ -9,6 +10,23 @@ app.controller('listVeiController',function($scope, $http){
             })
             .error(function(){
                 alert("Falha em obter dados");
-            }); 
+            });
+    }
     
+    $scope.listVeiculos();
+    
+    
+    $scope.delVeiculo = function(deletado){    
+        
+        if(!confirm("Tem certeza que deseja excluir?")) return false;
+        
+        $http.get('api/delVeiculo/'+deletado)
+            .success(function(data){
+                $scope.listVeiculos();
+            })
+            .error(function(){
+                alert("Falha em obter notícia");
+            });
+   
+    }
 });
